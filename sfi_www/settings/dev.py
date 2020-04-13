d8ecd6a9e3d4f832fcbff1934b984345c39387b3
Adam Pardyl <adam@pardyl.com>
2020-04-13 18:37:45 +0200

    Add wagtail and sfi_base

---
+++ sfi_www/settings/dev.py
@@ -0,0 +1,19 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '*q^o$d!6el@6vg-9ylnna5-t+@yvnraeyg5t6&!x=$5mh=x4%^'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+OIDC_ADMIN_ROLE = 'sfi'
+
+try:
+    from .local import *
+except ImportError:
+    pass
