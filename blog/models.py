4989ae940d6e2231821d25d710e28e8224518899
Adam Pardyl <adam@pardyl.com>
2020-04-23 21:41:27 +0200

    Add pagable PostIndex model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db import models
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import RichTextField
@@ -10,6 +11,22 @@ from common.models import SFIPage
 
 class PostIndex(SFIPage):
     subpage_types = ['Post']
+
+    POSTS_PER_PAGE = 10
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        all_posts = Post.objects.live().public().order_by('-date')
+        paginator = Paginator(all_posts, PostIndex.POSTS_PER_PAGE)
+        page = request.GET.get('page')
+        try:
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            posts = paginator.page(1)
+        except EmptyPage:
+            posts = paginator.page(paginator.num_pages)
+        context['posts'] = posts
+        return context
 
 
 class Post(SFIPage):
