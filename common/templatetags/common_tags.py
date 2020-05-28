0072534fc8a4c25121bb373743e6f48d18908612
Adam Pardyl <adam@pardyl.com>
2020-05-28 16:03:38 +0200

    Add page metadata

--- common/templatetags/common_tags.py
+++ common/templatetags/common_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from django.template.defaultfilters import stringfilter
+from django.template.defaultfilters import stringfilter, truncatewords
 from django.utils.html import strip_tags
 
 register = template.Library()
@@ -25,3 +25,31 @@ def responsive_img(img, size: str, css_class='', placeholder=''):
     else:
         # TODO: placeholder
         return {}
+
+
+@register.simple_tag
+def get_title(page):
+    if page.seo_title:
+        return page.seo_title
+    else:
+        return page.title
+
+
+@register.simple_tag
+def get_description(page):
+    if page.search_description:
+        return page.search_description
+    elif hasattr(page, 'content') and isinstance(page.content, str) and not page.content.startswith('{'):
+        return truncatewords(unrich_text(page.content), 15)
+    else:
+        return ''
+
+
+@register.simple_tag(takes_context=True)
+def get_featured_image(context, page):
+    if page.featured_image:
+        img = page.featured_image.get_rendition('max-1200x1200|format-jpeg|jpegquality-70')
+        return context.request.build_absolute_uri(img.url)
+    else:
+        # TODO: fallback
+        return ''
