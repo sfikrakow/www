a0af392868b61c078029c40d212ed0e975cfe1d5
szymonszoldra <szymon.szoldra@gmail.com>
2020-10-15 21:54:18 +0200

    added dropdown and dropdown logic

--- common/static/common/js/main.js
+++ common/static/common/js/main.js
@@ -7,4 +7,16 @@ hamburgerBtn.addEventListener('click', (e) => {
   navbarLinks.classList.toggle('is-active');
 });
 
+// Dropdown logic
+const dropdowns = [...document.querySelectorAll('.dropdown__title')];
 
+function handleDropdown() {
+  this.parentNode.classList.toggle('active');
+  const answer = this.nextElementSibling;
+  const height = `${answer.scrollHeight}px`;
+  answer.style.maxHeight = !answer.style.maxHeight ? height : null;
+}
+
+if(dropdowns) {
+  dropdowns.forEach(dropdown => dropdown.addEventListener('click', handleDropdown));
+}
