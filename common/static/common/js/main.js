d44c0bce756ea9033da4e67996a7006fa63871e2
Adam Pardyl <adam@pardyl.com>
2020-11-19 02:14:17 +0100

    Add CC workaround for grecaptcha

--- common/static/common/js/main.js
+++ common/static/common/js/main.js
@@ -1,6 +1,9 @@
 const hamburgerBtn = document.querySelector('.hamburger');
 const navbarLinks = document.querySelector('.navbar__links');
 
+// Hack for closure compiler. Replace with extern,
+var grecaptcha = {}
+
 // Toggle the hamburger button and the navbar links on click
 hamburgerBtn.addEventListener('click', (e) => {
     hamburgerBtn.classList.toggle('is-active');
