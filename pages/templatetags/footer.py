b2806a6c1395e8770750e1ed297296f6e2df9c90
Adam Pardyl <adam@pardyl.com>
2021-01-05 14:24:27 +0100

    Do not render empty streamfield as none

--- pages/templatetags/footer.py
+++ pages/templatetags/footer.py
@@ -8,6 +8,7 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def get_footer(context, page):
     if hasattr(page, 'get_edition_footer'):
-        return page.get_edition_footer()
+        footer = page.get_edition_footer()
     else:
-        return FooterSettings.for_request(context['request']).content
+        footer = FooterSettings.for_request(context['request']).content
+    return footer if footer else ''
