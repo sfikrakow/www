32d122f06d547d8e11b415529c8c74c57b665a58
Adam Pardyl <adam@pardyl.com>
2021-02-28 00:54:21 +0100

    Add blog post redirects

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,8 @@
-from datetime import datetime
-
 from django.db import models
+from django.shortcuts import redirect
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
 
@@ -32,15 +32,36 @@ class PostIndex(SFIPage):
 class Post(SFIPage):
     content = RichTextField(verbose_name=_('content'))
 
-    date = models.DateTimeField(default=datetime.now, verbose_name=_('post date'))
+    date = models.DateTimeField(default=timezone.now, verbose_name=_('post date'))
+
+    redirect_to = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        related_name="+",
+        on_delete=models.PROTECT,
+        verbose_name=_('redirect to page'),
+        help_text=_(
+            'Redirect to a specified page instead of showing content. '
+            'You should still add content or description to show in previews.')
+    )
 
     content_panels = SFIPage.content_panels + [
         FieldPanel('date'),
         FieldPanel('content'),
     ]
 
+    settings_panels = SFIPage.settings_panels + [
+        PageChooserPanel('redirect_to'),
+    ]
+
     parent_page_types = ['PostIndex']
     subpage_types = []
+
+    def serve(self, request, *args, **kwargs):
+        if self.redirect_to:
+            return redirect(self.redirect_to.url)
+        return super().serve(request, *args, **kwargs)
 
     class Meta:
         verbose_name = _('post')
